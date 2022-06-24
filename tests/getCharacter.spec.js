8d0ce8070aa128ca43049f1987b8bf3a52324519
Graziely Jacob <jacobgrazy@gmail.com>
2022-06-24 08:08:10 -0300

    Requisito 9 concluido

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -1,3 +1,4 @@
+
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 
@@ -43,13 +44,31 @@ Retorno:
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
-    // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
-    // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
-    // Teste se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele.
-    // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+  // ESCREVA SEUS TESTES ABAIXO:
+  // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    expect(getCharacter(undefined)).toBe(undefined);
+  // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
+    expect(getCharacter('Arya')).toEqual({
+      name: 'Arya Stark',
+      class: 'Rogue',
+      phrases: [ 'Not today', 'A girl has no name.' ]
+    })
+  // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
+    expect(getCharacter('Brienne')).toEqual({
+      name: 'Brienne Tarth',
+      class: 'Knight',
+      phrases:['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.']
+  })
+   // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
+  expect(getCharacter('Melissandre')).toEqual({
+    name: 'Melissandre',
+    class: 'Necromancer',
+    phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.']
+  })
+  // Teste se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele.
+  const lowered = 'MELISSANDRE'.toLowerCase();  
+  expect(lowered).toBe('melissandre');
+  // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+    expect(getCharacter('Afonso')).toEqual(undefined)
   });
 });
